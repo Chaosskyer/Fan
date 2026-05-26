95c6bc25b6423e8d990145a95446b56af04330b2
Lizhanhong <3012187388@qq.com>
2026-05-26 10:10:17 +0800

    初始创建

---
+++ User/src/test/java/lzh/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package lzh.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/lzh/fan/FanApplicationTests.java
@@ -0,0 +1,13 @@
+package lzh.fan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
